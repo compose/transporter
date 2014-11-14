c2254c9485b43c4ff163bc39ccadd65d2771ef79
Nick Stott <nick@nickstott.com>
2014-11-14 14:44:11 -0500

    add a run -eval

--- test/application.js
+++ test/application.js
@@ -6,8 +6,7 @@ t = Transporter()
 pipeline = Source({name:"crapfile", namespace: ""})
   .transform("transformers/passthrough_and_log.js")
   .save({name:"stdout", namespace: "c"})
-  // .transform("transformers/passthrough_and_log2.js")
-  // .save({name:"localmongo", namespace: "boom.crap"})
+
 
 // add the pipeline to the transporter
 t.add(pipeline)
