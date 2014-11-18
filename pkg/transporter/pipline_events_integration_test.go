a07c564779d2b75f3f8a227bde0f9da27a4a579e
JP <jp@mongohq.com>
2014-11-17 21:28:43 -0600

    lookup metric events by metric and path, dont assume proper order of receipt

--- pkg/transporter/pipline_events_integration_test.go
+++ pkg/transporter/pipline_events_integration_test.go
@@ -4,6 +4,7 @@ package transporter
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,6 +12,14 @@ import (
 	"testing"
 	"time"
 )
+
+var (
+	metricsEvents = make([][]byte, 0)
+)
+
+type EventHolder struct {
+	rawEvents [][]byte
+}
 
 func TestEventsBroadcast(t *testing.T) {
 	data := []struct {
@@ -35,11 +44,11 @@ func TestEventsBroadcast(t *testing.T) {
 		},
 	}
 
-	events := make([][]byte, 0)
+	eh := &EventHolder{rawEvents: make([][]byte, 0)}
 	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		event, _ := ioutil.ReadAll(r.Body)
 		r.Body.Close()
-		events = append(events, event)
+		eh.rawEvents = append(eh.rawEvents, event)
 	}))
 	defer ts.Close()
 	ts.Start()
@@ -81,21 +90,31 @@ func TestEventsBroadcast(t *testing.T) {
 
 	time.Sleep(time.Duration(5) * time.Second)
 
-	if len(events) != 4 {
-		t.Errorf("did not receive all events\nexp: %d\ngot: %d", 4, len(events))
+	if len(eh.rawEvents) != 4 {
+		t.Errorf("did not receive all events\nexp: %d\ngot: %d", 4, len(eh.rawEvents))
 	}
 
+	for _, val := range data {
+		if err = eh.lookupMetricEvent(val.evt, val.evtPath); err != nil {
+			t.Errorf("problem validating metric event, %s", err.Error())
+		}
+	}
+
+}
+
+func (events EventHolder) lookupMetricEvent(metric, path string) error {
 	var evt map[string]interface{}
-	for idx, val := range data {
-		if err = json.Unmarshal(events[idx], &evt); err != nil {
-			t.Errorf("unable to unmarshal event, %s", err.Error())
+	for _, val := range events.rawEvents {
+		if err := json.Unmarshal(val, &evt); err != nil {
+			return err
 		}
-		if evt["event"].(string) != val.evt {
-			t.Errorf("%s event should be %d event emitted, received %s\n", val.evt, idx, evt)
-		}
-		if val.evtPath != "" && evt["path"].(string) != val.evtPath {
-			t.Errorf("%s path incorrect, received %s\n", val.evtPath, evt)
+		if evt["event"].(string) == metric {
+			// check for path if provided
+			if path != "" && evt["path"].(string) == path {
+				return nil
+			}
+			return nil
 		}
 	}
-
+	return fmt.Errorf("unable to locate metric, %s, in received metric events", metric)
 }
