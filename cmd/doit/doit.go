892d4b1d0844abfcf9b9a6a5e8662f0ae387079d
JP <jp@mongohq.com>
2014-11-25 23:01:18 -0600

    attempt to fix merge issue

--- cmd/doit/doit.go
+++ cmd/doit/doit.go
@@ -8,11 +8,8 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
-=======
 	api := transporter.Api{MetricsInterval: 10000, Uri: "http://requestb.in/1b02tpv1"}
 
->>>>>>> c5b8df15673421ecb22840174624c33d08e42ee2
 	source := transporter.NewNode("name1", "mongo", map[string]interface{}{"uri": "mongodb://localhost/boom", "namespace": "boom.foo", "debug": true})
 	sink1 := transporter.NewNode("crapfile", "file", map[string]interface{}{"uri": "stdout://"})
 	sink2 := transporter.NewNode("crapfile2", "file", map[string]interface{}{"uri": "stdout://"})
