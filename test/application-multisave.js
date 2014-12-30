2cbb2f1420cf4f8ed4ab12b4c3b040238767b544
Dj Walker-Morgan <dj@codepope.com>
2014-12-30 10:41:36 +0000

    Make save() an alias of sink()

--- test/application-multisave.js
+++ test/application-multisave.js
@@ -1,5 +1,5 @@
 pipeline = Source({name:"foofile"})
 
-pipeline.transform({filename: "transformers/passthrough_and_log.js"}).save({name:"foofile2"})
-pipeline.save({name:"localmongo", namespace: "boom.bas"})
-pipeline.save({name:"localmongo", namespace: "boom.baz"})
+pipeline.transform({filename: "transformers/passthrough_and_log.js"}).sink({name:"foofile2"})
+pipeline.sink({name:"localmongo", namespace: "boom.bas"})
+pipeline.sink({name:"localmongo", namespace: "boom.baz"})
