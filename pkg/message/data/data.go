ca6c245b0db072dca730e7037c665d5d3f4882a0
Andrew Schwartz <andrew@compose.io>
2016-04-26 10:08:11 -0400

    marshal bson types into javascript editable types using mejson

--- pkg/message/data/data.go
+++ pkg/message/data/data.go
@@ -4,8 +4,20 @@ import "gopkg.in/mgo.v2/bson"
 
 type SQLData map[string]interface{}
 
+func (s SQLData) AsMap() map[string]interface{} {
+	return map[string]interface{}(s)
+}
+
 type BSONData bson.M
 
+func (b BSONData) AsMap() map[string]interface{} {
+	return map[string]interface{}(b)
+}
+
 type MapData map[string]interface{}
+
+func (m MapData) AsMap() map[string]interface{} {
+	return map[string]interface{}(m)
+}
 
 type CommandData string
