8c6b39097259bc8999f08767056390ae5c5a5a94
JP <jp@compose.io>
2017-02-15 15:43:34 -0600

    prep work test for running integration tests via cron

---
+++ scripts/before_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ev
+
+go get github.com/mattn/goveralls
+
+pip install "mongo-orchestration>=0.6.7,<1.0"
+
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-$MONGODB_VERSION.tgz
+
+mkdir -p /tmp/mongodb-linux-x86_64-ubuntu1404-$MONGODB_VERSION
+
+tar xfz mongodb-linux-x86_64-ubuntu1404-$MONGODB_VERSION.tgz -C /tmp
+
+rm mongodb-linux-x86_64-ubuntu1404-$MONGODB_VERSION.tgz
+
+export PATH=/tmp/mongodb-linux-x86_64-ubuntu1404-$MONGODB_VERSION/bin:$PATH
+
+mongod --version
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
-echo "mode: count" > /tmp/coverage.out
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -v -coverprofile=profile.out -covermode=count $d
-    if [ -f profile.out ]; then
-        cat profile.out | grep -v "mode: count" >> /tmp/coverage.out
-        rm profile.out
-    fi
-done
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+  echo "running integration tests"
+  go install ./cmd/transporter/...
+else
+  echo "running default tests"
+  
+  echo "mode: count" > /tmp/coverage.out
+
+  for d in $(go list ./... | grep -v vendor); do
+      go test -v -coverprofile=profile.out -covermode=count $d
+      if [ -f profile.out ]; then
+          cat profile.out | grep -v "mode: count" >> /tmp/coverage.out
+          rm profile.out
+      fi
+  done
+fi
