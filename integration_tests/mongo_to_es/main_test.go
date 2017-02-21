a277ad5e89deb490e932d42445f49c24a2abb0a1
JP <jp@compose.io>
2017-02-21 14:49:54 -0600

    better layout for integration tests
    use a matrix to run concurrently and use build tags to exclude integration tests

---
+++ integration_tests/mongo_to_es/main_test.go
@@ -0,0 +1,49 @@
+// +build integration
+
+package integration_test
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/compose/transporter/pkg/log"
+)
+
+var (
+	cleanup = flag.Bool("cleanup", false, "used to determien whether or not to run cleanup function")
+)
+
+func setup() {
+	log.Infoln("setting up tests")
+}
+
+func cleanupData() {
+	log.Infoln("cleaning up data")
+	req, _ := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/enron", os.Getenv("ES_ENRON_SINK_URI")), nil)
+	req.SetBasicAuth(os.Getenv("ES_ENRON_SINK_USER"), os.Getenv("ES_ENRON_SINK_PASSWORD"))
+	if _, err := http.DefaultClient.Do(req); err != nil {
+		log.Errorf("delete request errored, %s", err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	setup()
+	if *cleanup {
+		cleanupData()
+		shutdown()
+		os.Exit(0)
+		return
+	}
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
+
+func shutdown() {
+	log.Infoln("shutting down tests")
+	log.Infoln("tests shutdown complete")
+}
