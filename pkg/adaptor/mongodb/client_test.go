5a510843f156677172d7ce216309284a55f5bb9c
JP Phillips <jp@compose.io>
2017-02-21 09:27:01 -0600

    cleanup all sessions obtains from mgo (#272)
    we can never throw away sessions because mgo allocates a goroutine for each one and expects Close() to eventually be called
    
    fixes #270

--- pkg/adaptor/mongodb/client_test.go
+++ pkg/adaptor/mongodb/client_test.go
@@ -356,5 +356,8 @@ func TestConnect(t *testing.T) {
 		if err != ct.expectedErr {
 			t.Fatalf("[%s] unexpected Connect error, expected %+v, got %+v\n", ct.name, ct.expectedErr, err)
 		}
+		if err != nil {
+			ct.client.Close()
+		}
 	}
 }
