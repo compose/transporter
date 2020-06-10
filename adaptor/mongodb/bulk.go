ebf50a462cb27e3725e249582f50b477d6af7d27
Peter-May <peter.may@ibm.com>
2020-06-10 18:52:07 -0400

    adding more logging

--- adaptor/mongodb/bulk.go
+++ adaptor/mongodb/bulk.go
@@ -110,7 +110,7 @@ func (b *Bulk) run(done chan struct{}, wg *sync.WaitGroup) {
 				return
 			}
 		case <-done:
-			log.Debugln("received done channel")
+			log.Infoln("received done channel")
 			if err := b.flushAll(); err != nil {
 				log.Errorf("flush error, %s", err)
 			}
@@ -134,7 +134,7 @@ func (b *Bulk) flushAll() error {
 }
 
 func (b *Bulk) flush(c string, bOp *bulkOperation) error {
-	log.With("collection", c).With("opCounter", bOp.opCounter).With("bsonOpSize", bOp.bsonOpSize).Debugln("flushing bulk messages")
+	log.With("collection", c).With("opCounter", bOp.opCounter).With("bsonOpSize", bOp.bsonOpSize).Infoln("flushing bulk messages")
 	_, err := bOp.bulk.Run()
 	if err != nil && !mgo.IsDup(err) {
 		log.With("collection", c).Errorf("flush error, %s\n", err)
@@ -147,7 +147,7 @@ func (b *Bulk) flush(c string, bOp *bulkOperation) error {
 		}
 	}
 	bOp.s.Close()
-	log.With("collection", c).Debugln("flush complete")
+	log.With("collection", c).Infoln("flush complete")
 	delete(b.bulkMap, c)
 	return nil
 }
