6bd4988dcf7e6d20ae6b4088973f3377c3e762ed
Anthony Alberto <alberto.anthony@gmail.com>
2021-10-08 14:05:16 -0400

    Some script changes

--- setup_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.33.0
-go mod download
-
-./setup_db.sh
