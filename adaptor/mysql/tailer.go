091baa65af6571d16f163dbd3e922dbb74ea9432
atomicules <base@atomicules.co.uk>
2022-02-08 15:04:01 +0000

    Pretty much add/copy in files per Postgresl
    Might have updated a couple of comments. That's it.
    
    Will need a LOT of changes to work for MySQL

---
+++ adaptor/mysql/tailer.go
@@ -0,0 +1,234 @@
+package mysql
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/compose/transporter/client"
+	"github.com/compose/transporter/commitlog"
+	"github.com/compose/transporter/log"
+	"github.com/compose/transporter/message"
+	"github.com/compose/transporter/message/data"
+	"github.com/compose/transporter/message/ops"
+)
+
+var (
+	_ client.Reader = &Tailer{}
+)
+
+// Tailer implements the behavior defined by client.Tailer for interfacing with the MySQL binlog.
+type Tailer struct {
+	reader          client.Reader
+	replicationSlot string
+}
+
+func newTailer(replicationSlot string) client.Reader {
+	return &Tailer{newReader(), replicationSlot}
+}
+
+// Tail does the things
+func (t *Tailer) Read(resumeMap map[string]client.MessageSet, filterFn client.NsFilterFunc) client.MessageChanFunc {
+	return func(s client.Session, done chan struct{}) (chan client.MessageSet, error) {
+		readFunc := t.reader.Read(resumeMap, filterFn)
+		msgChan, err := readFunc(s, done)
+		if err != nil {
+			return nil, err
+		}
+		session := s.(*Session)
+		out := make(chan client.MessageSet)
+		go func() {
+			defer close(out)
+			// read until reader done
+			for msg := range msgChan {
+				out <- msg
+			}
+			// start tailing
+			log.With("db", session.db).With("logical_decoding_slot", t.replicationSlot).Infoln("Listening for changes...")
+			for {
+				select {
+				case <-done:
+					log.With("db", session.db).Infoln("tailing stopping...")
+					return
+				case <-time.After(time.Second):
+					msgSlice, err := t.pluckFromLogicalDecoding(s.(*Session), filterFn)
+					if err != nil {
+						log.With("db", session.db).Errorf("error plucking from logical decoding %v", err)
+						continue
+					}
+					for _, msg := range msgSlice {
+						out <- msg
+					}
+				}
+			}
+		}()
+
+		return out, nil
+	}
+}
+
+// Use MySQL binlog to retrieve the latest changes
+func (t *Tailer) pluckFromLogicalDecoding(s *Session, filterFn client.NsFilterFunc) ([]client.MessageSet, error) {
+	var result []client.MessageSet
+	dataMatcher := regexp.MustCompile(`(?s)^table ([^\.]+)\.([^:]+): (INSERT|DELETE|UPDATE): (.+)$`) // 1 - schema, 2 - table, 3 - action, 4 - remaining
+
+	changesResult, err := s.mysqlSession.Query(fmt.Sprintf("SELECT * FROM pg_logical_slot_get_changes('%v', NULL, NULL);", t.replicationSlot))
+	if err != nil {
+		return result, err
+	}
+
+	for changesResult.Next() {
+		var (
+			location string
+			xid      string
+			d        string
+		)
+
+		err = changesResult.Scan(&location, &xid, &d)
+		if err != nil {
+			return result, err
+		}
+
+		// Ensure we are getting a data change row
+		dataMatches := dataMatcher.FindStringSubmatch(d)
+		if len(dataMatches) == 0 {
+			continue
+		}
+
+		// Skippable because no primary key on record
+		// Make sure we are getting changes on valid tables
+		schemaAndTable := fmt.Sprintf("%v.%v", dataMatches[1], dataMatches[2])
+		if !filterFn(schemaAndTable) {
+			continue
+		}
+
+		if dataMatches[4] == "(no-tuple-data)" {
+			log.With("op", dataMatches[3]).With("schema", schemaAndTable).Infoln("no tuple data")
+			continue
+		}
+
+		// normalize the action
+		var action ops.Op
+		switch dataMatches[3] {
+		case "INSERT":
+			action = ops.Insert
+		case "DELETE":
+			action = ops.Delete
+		case "UPDATE":
+			action = ops.Update
+		default:
+			return result, fmt.Errorf("Error processing action from string: %v", d)
+		}
+
+		log.With("op", action).With("table", schemaAndTable).Debugln("received")
+
+		docMap := parseLogicalDecodingData(dataMatches[4])
+		result = append(result, client.MessageSet{
+			Msg:  message.From(action, schemaAndTable, docMap),
+			Mode: commitlog.Sync,
+		})
+	}
+
+	return result, err
+}
+
+func parseLogicalDecodingData(d string) data.Data {
+	data := make(data.Data)
+	var (
+		label                  string
+		labelFinished          bool
+		valueType              string
+		valueTypeFinished      bool
+		openBracketInValueType bool
+		skippedColon           bool
+		value                  string // will type switch later
+		valueEndCharacter      string
+		deferredSingleQuote    bool
+		valueFinished          bool
+	)
+
+	valueTypeFinished = false
+	labelFinished = false
+	skippedColon = false
+	deferredSingleQuote = false
+	openBracketInValueType = false
+	valueFinished = false
+
+	for _, character := range d {
+		if !labelFinished {
+			if string(character) == "[" {
+				labelFinished = true
+				continue
+			}
+			label = fmt.Sprintf("%v%v", label, string(character))
+			continue
+		}
+		if !valueTypeFinished {
+			if openBracketInValueType && string(character) == "]" { // if a bracket is open, close it
+				openBracketInValueType = false
+			} else if string(character) == "]" { // if a bracket is not open, finish valueType
+				valueTypeFinished = true
+				continue
+			} else if string(character) == "[" {
+				openBracketInValueType = true
+			}
+			valueType = fmt.Sprintf("%v%v", valueType, string(character))
+			continue
+		}
+
+		if !skippedColon && string(character) == ":" {
+			skippedColon = true
+			continue
+		}
+
+		if len(valueEndCharacter) == 0 {
+			if string(character) == "'" {
+				valueEndCharacter = "'"
+				continue
+			}
+			valueEndCharacter = " "
+		}
+
+		// ending with '
+		if deferredSingleQuote && string(character) == " " { // we hit an unescaped single quote
+			valueFinished = true
+		} else if deferredSingleQuote && string(character) == "'" { // we hit an escaped single quote ''
+			deferredSingleQuote = false
+		} else if string(character) == "'" && !deferredSingleQuote { // we hit a first single quote
+			deferredSingleQuote = true
+			continue
+		}
+
+		// ending with space
+		if valueEndCharacter == " " && string(character) == valueEndCharacter {
+			valueFinished = true
+		}
+
+		// continue parsing
+		if !valueFinished {
+			value = fmt.Sprintf("%v%v", value, string(character))
+			continue
+		}
+
+		// Set and reset
+		data[label] = casifyValue(value, valueType)
+
+		label = ""
+		labelFinished = false
+		valueType = ""
+		valueTypeFinished = false
+		skippedColon = false
+		deferredSingleQuote = false
+		value = ""
+		valueEndCharacter = ""
+		valueFinished = false
+	}
+	if len(label) > 0 { // ensure we process any line ending abruptly
+		data[label] = casifyValue(value, valueType)
+	}
+	return data
+}
---
+++ adaptor/mysql/tailer_test.go
@@ -0,0 +1,112 @@
+package mysql
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/compose/transporter/client"
+)
+
+func addTestReplicationSlot(s *sql.DB) error {
+	_, err := s.Exec(`
+    SELECT * FROM pg_create_logical_replication_slot('test_slot', 'test_decoding');
+  `)
+	return err
+}
+
+func dropTestReplicationSlot(s *sql.DB) error {
+	_, err := s.Exec(`
+    SELECT * FROM pg_drop_replication_slot('test_slot');
+  `)
+	return err
+}
+
+var (
+	tailerTestData = &TestData{"tailer_test", "tailer_test_table", basicSchema, 10}
+)
+
+func TestTailer(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping Tailer in short mode")
+	}
+	c, err := NewClient(WithURI(fmt.Sprintf("mysql://127.0.0.1:5432/%s?sslmode=disable", tailerTestData.DB)))
+	if err != nil {
+		t.Fatalf("unable to initialize connection to mysql, %s", err)
+	}
+	defer c.Close()
+	s, err := c.Connect()
+	if err != nil {
+		t.Fatalf("unable to obtain session to mysql, %s", err)
+	}
+
+	dropTestReplicationSlot(s.(*Session).mysqlSession)
+	if err := addTestReplicationSlot(s.(*Session).mysqlSession); err != nil {
+		t.Fatalf("unable to create replication slot, %s", err)
+	}
+	time.Sleep(1 * time.Second)
+
+	r := newTailer("test_slot")
+	readFunc := r.Read(map[string]client.MessageSet{}, func(table string) bool {
+		if strings.HasPrefix(table, "information_schema.") || strings.HasPrefix(table, "pg_catalog.") {
+			return false
+		}
+		return table == fmt.Sprintf("public.%s", tailerTestData.Table)
+	})
+	done := make(chan struct{})
+	msgChan, err := readFunc(s, done)
+	if err != nil {
+		t.Fatalf("unexpected Read error, %s\n", err)
+	}
+	checkCount("initial drain", tailerTestData.InsertCount, msgChan, t)
+
+	for i := 10; i < 20; i++ {
+		s.(*Session).mysqlSession.Exec(fmt.Sprintf(`INSERT INTO %s VALUES (
+      %d,            -- id
+      '%s',          -- colvar VARCHAR(255),
+      now() at time zone 'utc' -- coltimestamp TIMESTAMP,
+    );`, tailerTestData.Table, i, randomHeros[i%len(randomHeros)]))
+	}
+	checkCount("tailed data", 10, msgChan, t)
+
+	for i := 10; i < 20; i++ {
+		s.(*Session).mysqlSession.Exec(fmt.Sprintf("UPDATE %s SET colvar = 'hello' WHERE id = %d;", tailerTestData.Table, i))
+	}
+	checkCount("updated data", 10, msgChan, t)
+
+	for i := 10; i < 20; i++ {
+		s.(*Session).mysqlSession.Exec(fmt.Sprintf(`DELETE FROM %v WHERE id = %d; `, tailerTestData.Table, i))
+	}
+
+	checkCount("deleted data", 10, msgChan, t)
+
+	close(done)
+}
+
+func checkCount(desc string, expected int, msgChan <-chan client.MessageSet, t *testing.T) {
+	var numMsgs int
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		for {
+			select {
+			case <-msgChan:
+				numMsgs++
+			case <-time.After(1 * time.Second):
+				if numMsgs == expected {
+					wg.Done()
+					return
+				}
+			case <-time.After(20 * time.Second):
+				wg.Done()
+				return
+			}
+		}
+	}(&wg)
+	wg.Wait()
+	if numMsgs != expected {
+		t.Errorf("[%s] bad message count, expected %d, got %d\n", desc, expected, numMsgs)
+	}
+}
